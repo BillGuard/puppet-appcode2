d15ed1910d200bb6993a4e17b858816fd174cad1
nirgin <nir.ginzler@gmail.com>
2016-01-24 18:25:21 +0200

    updated appcode version and link, default is now 3.3.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,11 +10,11 @@
 #   version => 2.0, 
 # }
 class appcode2 (
-  $version = '2.0',
+  $version = '3.3.2',
 ) {
   require java
   package { 'AppCode2':
     provider => 'appdmg',
-    source   => "http://download.jetbrains.com/objc/AppCode-${version}.dmg",
+    source   => "http://download.jetbrains.com/objc/AppCode-${version}-custom-jdk-bundled.dmg",
   }
 }
